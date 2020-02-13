1977368384ff8b9b08be4dd83c761d4e522637a2
Patrick Kennedy <patkennedy79@gmail.com>
2020-02-12 21:25:58 -0800

    Added instance folder for storing configuration

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ from flask import Flask, escape, render_template, request, session, redirect, ur
 from logging.handlers import RotatingFileHandler
 import logging
 
-app = Flask(__name__)
+# Create the Flask application
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_pyfile('flask.cfg', silent=False)
+app.config.from_pyfile('flask_sensitive.cfg', silent=True)
 
 # blueprints
 from project.stocks import stocks_blueprint
@@ -21,51 +24,3 @@ file_handler.setFormatter(file_formatter)
 file_handler.setLevel(logging.INFO)
 app.logger.addHandler(file_handler)
 app.logger.info('Starting the Flask Stock Portfolio App...')
-
-# TEMPORARY - Set the secret key to a temporary value!
-app.secret_key = 'BAD_SECRET_KEY'
-
-
-# @app.route('/')
-# def index():
-#     app.logger.info('Calling the index() function.')
-#     return render_template('index.html')
-#
-#
-# @app.route('/about')
-# def about():
-#     flash('Thanks for learning about this site!', 'info')
-#     return render_template('about.html', company_name='TestDriven.io')
-#
-#
-# @app.route('/users/<username>')
-# def user_profile(username):
-#     return f'<h1>Welcome {escape(username)}!</h1>'
-#
-#
-# @app.route('/blog_posts/<int:post_id>')
-# def display_blog_post(post_id):
-#     return f'<h1>Blog Post #{post_id}...</h1>'
-#
-#
-# @app.route('/add_stock', methods=['GET', 'POST'])
-# def add_stock():
-#     if request.method == 'POST':
-#         # DEBUG - Print the form data to the console
-#         for key, value in request.form.items():
-#             print(f'{key}: {value}')
-#
-#         # Save the form data to the session object
-#         session['stockSymbol'] = request.form['stockSymbol']
-#         session['numberOfShares'] = request.form['numberOfShares']
-#         session['sharePrice'] = request.form['sharePrice']
-#         flash(f"Added new stock ({ request.form['stockSymbol'] })!", 'success')
-#         app.logger.info(f"Added new stock ({ request.form['stockSymbol'] })!")
-#         return redirect(url_for('list_stocks'))
-#     else:
-#         return render_template('add_stock.html')
-#
-#
-# @app.route('/stocks')
-# def list_stocks():
-#     return render_template('stocks.html')
