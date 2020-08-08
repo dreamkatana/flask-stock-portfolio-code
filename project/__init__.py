772b1d91b5280fd1a01a4a0d99aee7db27f8e485
Patrick Kennedy <patkennedy79@gmail.com>
2020-08-08 13:41:35 -0700

    Update to logger initialization

--- project/__init__.py
+++ project/__init__.py
@@ -96,8 +96,8 @@ def configure_logging(app):
         file_handler.setLevel(logging.INFO)
         app.logger.addHandler(file_handler)
 
-    # Remove the default logger configured by Flask
-    app.logger.removeHandler(default_handler)
+        # Remove the default logger configured by Flask
+        app.logger.removeHandler(default_handler)
 
     app.logger.info('Starting the Flask Stock Portfolio App...')
 
