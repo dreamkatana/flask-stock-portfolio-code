4c8d3f678887775b6a4d40a86e5dbece1bb06b6a
Patrick Kennedy <pkennedy@hey.com>
2020-11-25 17:07:27 -0800

    Updates based on implementing code for chapter 12 (blueprints)

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-import logging
+from flask import Flask
 from logging.handlers import RotatingFileHandler
-from flask import Flask, escape, render_template, request, session, redirect, url_for, flash
+import logging
 from flask.logging import default_handler
 
 app = Flask(__name__)
@@ -24,46 +24,10 @@ app.logger.removeHandler(default_handler)
 app.logger.info('Starting the Flask Stock Portfolio App...')
 
 
-@app.route('/')
-def index():
-    app.logger.info('Calling the index() function.')
-    return render_template('index.html')
+# Import the blueprints
+from project.stocks import stocks_blueprint
+from project.users import users_blueprint
 
-
-@app.route('/about')
-def about():
-    flash('Thanks for learning about this site!', 'info')
-    return render_template('about.html', company_name='TestDriven.io')
-
-
-@app.route('/hello/<message>')
-def hello_message(message):
-    return f'<h1>Welcome {escape(message)}!</h1>'
-
-
-@app.route('/blog_posts/<int:post_id>')
-def display_blog_post(post_id):
-    return f'<h1>Blog Post #{post_id}...</h1>'
-
-
-@app.route('/add_stock', methods=['GET', 'POST'])
-def add_stock():
-    if request.method == 'POST':
-        # Print the form data to the console
-        for key, value in request.form.items():
-            print(f'{key}: {value}')
-
-        # Save the form data to the session object
-        session['stock_symbol'] = request.form['stock_symbol']
-        session['number_of_shares'] = request.form['number_of_shares']
-        session['purchase_price'] = request.form['purchase_price']
-        flash(f"Added new stock ({request.form['stock_symbol']})!", 'success')
-        app.logger.info(f"Added new stock ({ request.form['stock_symbol'] })!")
-        return redirect(url_for('list_stocks'))
-
-    return render_template('add_stock.html')
-
-
-@app.route('/stocks/')
-def list_stocks():
-    return render_template('stocks.html')
+# Register the blueprints
+app.register_blueprint(stocks_blueprint)
+app.register_blueprint(users_blueprint, url_prefix='/users')
