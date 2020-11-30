84954ad65f0810458f4d8c5efba668ad6e6fcc88
Patrick Kennedy <pkennedy@hey.com>
2020-11-30 08:31:33 -0800

    Another attempt at fixing GitLab CI issue with Flask-Mail

--- config.py
+++ config.py
@@ -42,6 +42,5 @@ class TestingConfig(Config):
                                         default=f"sqlite:///{os.path.join(BASEDIR, 'instance', 'test.db')}")
     BCRYPT_LOG_ROUNDS = 4
     WTF_CSRF_ENABLED = False
-    MAIL_USERNAME = 'pkennedy@hey.com'
-    MAIL_PASSWORD = 'not_used_for_testing'
-    MAIL_DEFAULT_SENDER = 'pkennedy@hey.com'
+    MAIL_SUPPRESS_SEND = True
+
